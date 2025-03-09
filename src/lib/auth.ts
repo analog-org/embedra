43f10d7db91bd9c0f536ea397fcc2e3a335c8f55
BossDaily <gboys2008@gmail.com>
2025-03-08 21:29:05 -0600

    fixed auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,8 +3,8 @@ import { betterAuth } from "better-auth";
 export const auth = betterAuth({
   socialProviders: {
     discord: {
-      clientId: process.env.DISCORD_CLIENT_ID as string,
-      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      clientId: import.meta.env.DISCORD_CLIENT_ID as string,
+      clientSecret: import.meta.env.DISCORD_CLIENT_SECRET as string,
       scope: ["identify", "guilds"]
     },
   },
