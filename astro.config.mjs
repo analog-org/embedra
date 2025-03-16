80c5c83ea67a16c1caa5658df382d929642fd6ac
BossDaily <gboys2008@gmail.com>
2025-03-16 16:56:54 -0500

    new integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server', // Enable SSR mode
+  integrations: [
+    sentry(),
+    spotlightjs(),
+    react({
+      include: ["**/react/*"],
+    }),
+  ],
+  output: "server", // Enable SSR mode
   vite: {
     plugins: [tailwindcss()],
   },
 });
-
-
